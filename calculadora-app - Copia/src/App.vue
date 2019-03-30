d99df61cb557a91dc54971946c96b7128a58d416
glaucia86 <glaucia_lemos86@hotmail.com>
2019-03-30 11:42:46 -0300

    Envio de uma outra demo - Calculadora melhorada

---
+++ calculadora-app - Copia/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Calculadora/>
+  </div>
+</template>
+
+<script>
+// import Calculadora from './components/Calculadora';
+import Calculadora from './components/Calculadora/index';
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
